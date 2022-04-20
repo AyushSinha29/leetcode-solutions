17d93e6bf29235f642f6f89d576293bc7b44fe7d
Ayush Sinha <55970505+AyushSinha29@users.noreply.github.com>
2022-04-21 01:15:38 +0530

    Time: 51 ms, Memory: 19.9 MB - LeetHub

--- design-linked-list/design-linked-list.cpp
+++ design-linked-list/design-linked-list.cpp
@@ -1,6 +1,3 @@
-// Time:  O(n)
-// Space: O(n)
-
 struct Node {
     Node(int value) :
          val(value),
@@ -13,14 +10,14 @@ struct Node {
 
 class MyLinkedList {
 public:
-    /** Initialize your data structure here. */
+    
     MyLinkedList() : size_(0) {
         head_ = tail_ = new Node(-1);
         head_->next = tail_;
         tail_->prev = head_;
     }
     
-    /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
+    
     int get(int index) {
         if (0 <= index && index <= size_ / 2) {
             return forward(0, index, head_->next)->val;
@@ -30,21 +27,16 @@ public:
         return -1;
     }
     
-    /** Add a node of value val before the first element of the linked list.
-        After the insertion, the new node will be the first node of the linked list. */
     void addAtHead(int val) {
         add(head_, val);
     }
     
-    /** Append a node of value val to the last element of the linked list. */
+   
     void addAtTail(int val) {
         add(tail_->prev, val);
     }
     
-    /** Add a node of value val before the index-th node in the linked list.
-        If index equals to the length of linked list,
-        the node will be appended to the end of linked list.
-        If index is greater than the length, the node will not be inserted. */
+   
     void addAtIndex(int index, int val) {
         if (0 <= index && index <= size_ / 2) {
             add(forward(0, index, head_->next)->prev, val);
@@ -53,7 +45,7 @@ public:
         }
     }
     
-    /** Delete the index-th node in the linked list, if the index is valid. */
+
     void deleteAtIndex(int index) {
         if (0 <= index && index <= size_ / 2) {
             remove(forward(0, index, head_->next));
@@ -98,13 +90,5 @@ private:
     int size_;
 };
 
-/**
- * Your MyLinkedList object will be instantiated and called as such:
- * MyLinkedList obj = new MyLinkedList();
- * int param_1 = obj.get(index);
- * obj.addAtHead(val);
- * obj.addAtTail(val);
- * obj.addAtIndex(index,val);
- * obj.deleteAtIndex(index);
- */
+
  
