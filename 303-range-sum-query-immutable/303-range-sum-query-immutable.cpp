711790410bfdcc9beb6745264a1d826b326e055e
Ayush Sinha <55970505+AyushSinha29@users.noreply.github.com>
2022-07-31 23:50:33 +0530

    Time: 455 ms (10.39%), Space: 17.2 MB (32.79%) - LeetHub

--- 303-range-sum-query-immutable/303-range-sum-query-immutable.cpp
+++ 303-range-sum-query-immutable/303-range-sum-query-immutable.cpp
@@ -1,14 +1,22 @@
+
 class NumArray {
-private:
-    vector<int> v;
+    vector<int> ans;
 public:
     NumArray(vector<int>& nums) {
-        v = nums;
+        
+        for(int i=0;i<nums.size();i++){
+            
+            ans.push_back(nums[i]);
+        }
+        
+        
     }
     
     int sumRange(int left, int right) {
-        int sum = 0;
-        for(int i = left; i <= right; ++i) sum += v[i];
+        
+        int sum=0;
+        for(int i=left;i<=right;i++) sum+=ans[i];
+        
         return sum;
     }
 };
