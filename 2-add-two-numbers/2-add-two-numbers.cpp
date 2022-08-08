74b10ab7c7df457be1c495c6401d2543fc884a23
Ayush Sinha <55970505+AyushSinha29@users.noreply.github.com>
2022-08-09 00:47:48 +0530

    Time: 32 ms (95.08%), Space: 71.5 MB (51.04%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -1,37 +1,41 @@
+
 class Solution {
 public:
-ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-
-    int carry=0;
-    ListNode *t1=l1, *t2=l2, *lastNode=t1;
-    
-    while(t1!=NULL && t2!=NULL){
-        int value= t1->val + t2->val + carry;
-        t1->val= value%10;
-        carry= value/10;
-        lastNode =t1;
-        t1=t1->next;
-        t2=t2->next;
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *ptr = new ListNode();     //new list;
+        ListNode *temp = ptr;
+        
+        int c = 0;
+		//traversing both list till one of the list not reaches NULL
+        while (l1 != NULL ||  l2 != NULL || c)
+        {
+            int sum = 0;
+			// if l1  is not null
+			// add l1-> value to sum
+            if(l1 != NULL)
+            {
+                sum += l1->val;
+                l1 = l1 -> next;
+            }
+            
+			// if l2  is not null
+			// add l2-> value to sum
+            if(l2 != NULL)
+            {
+                sum += l2->val;
+                l2 = l2 -> next;
+            }
+            
+			// add carry to sum
+            sum += c;
+			// carry is updated by sum/10 because for 18 , 
+			// 18 / 10 is 1 which is the carry
+            c = sum/10;
+			// add sum% 10 to new node as it containg the sum
+            ListNode *node = new ListNode(sum%10);
+            temp -> next = node;
+            temp = temp -> next;
+        }
+        return ptr -> next;
     }
-    
-    if( t2!=NULL ){
-        lastNode->next=t2;
-        t1=lastNode->next;
-    }
-    
-    while( t1!=NULL ){
-        int value= t1->val + carry;
-        t1->val= value%10;
-        carry= value/10;
-        lastNode =t1;
-        t1= t1->next;
-    }
-    
-    if(carry!=0){
-        ListNode *node=new ListNode(carry);
-        lastNode->next= node;
-    }
-    
-    return l1;
-}
 };
