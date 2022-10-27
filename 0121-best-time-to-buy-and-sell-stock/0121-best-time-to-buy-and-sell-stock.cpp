54e3dc65c8a3a4ad84cb65786a33a58aa775253f
Ayush Sinha <sinhaaayush0829@gmail.com>
2022-10-27 22:21:10 +0530

    Time: 357 ms (21.16%), Space: 93.4 MB (10.00%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int profit = 0, pointer = prices[0];
+        
+        for(int i = 1; i < n; i++) {
+            if(prices[i] < pointer) 
+                
+                pointer = prices[i];  //fixing lower bound
+            
+            else 
+                profit = max(profit, prices[i] - pointer);
+        }
+        
+        return profit;
+    }
+};
